4ed280b6f7b88b486aecd9f7d0314b353b7a2814
LiafanX <liafanx@gmail.com>
2026-09-26 10:27:03 +0000

    Release MTProxyL 1.6.24 and panel 1.2.8

--- mtproxyl-panel/main.go
+++ mtproxyl-panel/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Liafanx/mtproxyl-panel/internal/server"
 )
 
-var version = "1.2.7"
+var version = "1.2.8"
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "version" {
